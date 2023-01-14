c8998fdad4726a22d4e87a5c9c0d09ab691160c2
animih <anisimov.mi@phystech.edu>
2023-01-14 10:04:16 +0300

    Implemented optimal brain damage

--- weight_regularization.py
+++ weight_regularization.py
@@ -27,12 +27,12 @@ def l2_norm(list_of_parameters, alpha = 1):
 
     return reg_cost
 
-def l2_super_norm(list_of_parameters, alpha = 1, beta = 1):
+def l2_s_norm(list_of_parameters, alpha = 1, beta = 1):
     reg_cost = 0
     for parameters in list_of_parameters:
         reg_cost += alpha * (
-            beta * beta * ( parameters**2 / (1 + parameters**2)).sum()
-            / torch.numel(parameters)
+            beta * ( parameters**2 / (1 + parameters**2) ).sum()
+            / torch.numel(parameters) +
             (1-beta) * (parameters**2).sum() / torch.numel(parameters)
             )
 
