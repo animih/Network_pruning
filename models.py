378f901f6e634d3e7d324a3920b06afe087ab9b6
animih <anisimov.mi@phystech.edu>
2023-01-26 17:42:06 +0300

    added a study for ResNet-like network

--- models.py
+++ models.py
@@ -24,6 +24,72 @@ class LeNet(nn.Module):
         x = self.fc3(x)
         return F.log_softmax(x, 1)
 
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
+
+        self.res1 = nn.Sequential(nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        ), nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True))
+        )
+        
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
+
+        self.res2 = nn.Sequential(nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        ), nn.Sequential( 
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True))
+        )
+
+        self.classifier = nn.Sequential(
+            nn.MaxPool2d(4), 
+            nn.Flatten(), 
+            nn.Linear(512, 10)
+        )
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.res1(x) + x
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.res2(x) + x
+        x = self.classifier(x)
+        
+        return x
 
 def train_model(model, optimizer, crit, loaders,
     reg = None, display = False,
